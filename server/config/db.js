3ae2738482d34f4d1ff4f4d4c5376f7325988f7a
mbhadani001 <105386899+mbhadani001@users.noreply.github.com>
2022-06-25 16:45:11 -0400

    URI updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 module.exports = {
   
   //local MongoDB deployment ->
-  "URI": "mongodb://127.0.0.1:27017/midterm229"
+  // "URI": "mongodb://127.0.0.1:27017/midterm229"
+  "URI": "mongodb+srv://movies:yhi6RI07jM1rsYqG@cluster0.rqyncro.mongodb.net/movies?retryWrites=true&w=majority"
   
 };
